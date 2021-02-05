67d776d1689c9b07b7343de67b27560886897eb5
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-02-05 15:12:12 -0800

    assets added

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame # 2D Graphics Library
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+WHITE = (255,255,255)
+
+FPS = 60
+
+def draw_window():
+		WIN.fill(WHITE)
+		pygame.display.update()
+
+def main():
+	clock = pygame.time.Clock()
+	run = True
+	while run:
+		clock.tick(FPS)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		draw_window()
+
+
+
+	pygame.quit()
+
+if __name__ == "__main__":
+	main()
